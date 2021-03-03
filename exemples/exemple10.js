8ed916ad6a885d8ac50320d26d97ed150d97b3b0
Joris T <77294422+AoRoxas@users.noreply.github.com>
2021-03-03 22:22:44 +0100

    commit

---
+++ exemples/exemple10.js
@@ -0,0 +1,72 @@
+
+
+
+let rue = "Place de la Concorde"; //Chaîne de caractère//
+console.log(rue)
+
+let âge = 18; //Number//
+console.log(âge)
+
+let majeur = true; //Boolean//
+console.log(majeur)
+
+let personne = {//objet
+    nom : 'Vilain'
+    prénom : 'Guy'
+    âge : '41'
+}
+
+console.log(personne)
+
+console.log(personne.nom + " " + personne.prénom + " " + personne.âge);
+
+function direBonjour(){console.log("Bonjour");}//déclarer une fonction//
+
+direBonjour();//Appel de ma fonction
+
+
+
+
+
+const prénom = "Guy"
+
+console.log(prénom);
+
+let salutation = "Bonjour" + prénom;
+
+console.log ("Mais qui voilà ??? \n" + salutation)
+
+const rencontre = "je suis vraiment heureux"
+
+console.log(`Mais qui voilà??? ${salutation}, \n ${rencontre}`);
+
+
+
+
+
+let nombre1 = 8, nombre2 = 4, nombre3 = 12, résultat;
+
+//ADDITION
+résultat = nombre3 + nombre2;
+console.log('le résultat de l\'addition est : ' + résultat)
+
+
+//SOUSTRACTION
+résultat = nombre3 - nombre2;
+console.log(`${nombre3}/ ${nombre2}=${résultat}`);
+
+//DIVISION
+résultat = nombre1 / nombre2;
+console.log(`${nombre3}/ ${nombre2}=${résultat}`);
+
+//MULTIPLICATION
+résultat = nombre1 * nombre3;
+console.log(`${nombre1} X ${nombre3}=${résultat}`);
+
+//MODULO
+résultat = 5%2
+console.log(`le résultat de 5 Modulo 2 est = ` + résultat);
+
+//EXPONENTIELLE
+résultat = 5**2;
+console.log(`le résultat de 5 exp 2 est = ${résultat}`);

7419930117b5f0779b850f97182d39ba3d9dfd0d
Joris T <77294422+AoRoxas@users.noreply.github.com>
2021-03-05 00:01:29 +0100

    commit

---
+++ pratique/exercice3.js
@@ -0,0 +1,34 @@
+let nom = prompt ("Quel votre nom ?");
+let prenom = prompt("Quel est votre prénom ?");
+let age = prompt("Quel est votre âge ?")
+let adresse = prompt ("Quel est votre adresse ?");
+let cp = prompt ("Quel est votre code postal ?");
+let ville = prompt ("Quel est votre ville ?");
+let taille = prompt ("Quel est votre taille (en mètre Ex : 1.70) ?");
+let poids = prompt ("Quel est votre poids (en Kg) ?");
+let resultat = poids / taille**2
+
+function start(){
+    let div = document.getElementById('container');                 
+    let p = div.appendChild(document.createElement("p"));
+    p.appendChild(document.createTextNode(`Nom : ${nom} Prénom : ${prenom} 
+    Age : ${age} Adresse : ${adresse} Code Postal : ${cp} Ville : ${ville}
+    Taille : ${taille}m Poids : ${poids} IMC : ${resultat}`));
+}
+
+
+
+
+
+
+
+// document.write(`Nom : ${nom}`);
+// document.write(`Prénom :  ${prenom}`);
+// document.write(`Adresse : ${adresse}`);
+// document.write(`Code Postal : ${cp}`);
+// document.write(`Ville :  ${ville}`);
+// document.write(`Taille :  ${taille}m`);
+// document.write(`Poids : ${poids}`);
+// document.write(`IMC : ${resultat}`);
+
+    

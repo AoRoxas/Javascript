00caf12aaa5fb799102f40923ce3690af16728b9
Joris T <77294422+AoRoxas@users.noreply.github.com>
2021-03-16 20:46:59 +0100

    commit

--- pratique/challenge/challenge04.js
+++ pratique/challenge/challenge04.js
@@ -2,24 +2,31 @@
 do{
     nombre1 = parseInt(window.prompt("Choississez votre premier nombre"));
     nombre2 = parseInt(window.prompt("Choississez votre deuxième nombre"));
- }while (isNaN(nombre1) == Number);
- while (isNaN (nombre2) == Number);
+}
+while (isNaN(nombre1) == Number);
+while (isNaN (nombre2) == Number);
 
+    if (nombre1 || nombre2 !== Number) {
+       alert = ("Veillez entrer des nombres !")
+       
+    };
+
+    
 try {
-    let operation = prompt('Choissez votre opération : addition - soustraction - multiplication - division');
+    let operation = prompt('Choissez votre opération :\r 1. Addition \r 2. Soustraction \r 3. Multiplication \r 4. Division');
     let resultat;
 
     switch (operation) {
-        case 'addition':
+        case '1':
             resultat = nombre1 + nombre2
             break;
-        case 'soustraction' : 
+        case '2' : 
         resultat = nombre1 - nombre2
             break;
-        case 'multiplication' :
+        case '3' :
             resultat = nombre1 * nombre2
             break;
-        case 'division' : 
+        case '4' : 
         resultat = nombre1 / nombre2
             break;
         default :
